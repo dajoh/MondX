a56574bfdc05c6d138f08bf5da05abd510680572
Daniel Lindberg <dajoh10@gmail.com>
2015-02-06 07:52:07 +0100

    Added color to Unix fancy printer.

--- MondX/DiagPrinterFancyUnix.cpp
+++ MondX/DiagPrinterFancyUnix.cpp
@@ -8,8 +8,20 @@ DiagPrinterFancy::DiagPrinterFancy(Source &source) : DiagPrinterFancyCore(source
 
 void DiagPrinterFancy::SetColor(Severity s)
 {
+	switch (s) {
+	case Info:
+		printf("\x1B[1;36m");
+		break;
+	case Warning:
+		printf("\x1B[1;33m");
+		break;
+	case Error:
+		printf("\x1B[1;31m");
+		break;
+	}
 }
 
 void DiagPrinterFancy::ResetColor()
 {
+		printf("\x1B[0m");
 }
